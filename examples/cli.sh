d9afe89e8d83d745c31fc1be6d09ce5163120cfa
Ana Nelson <ana@ananelson.com>
2013-10-25 17:57:31 -0700

    Update docs and examples.

---
+++ examples/cli.sh
@@ -0,0 +1,17 @@
+### "help"
+oacensus help
+
+### "help-run"
+oacensus help -on run
+
+### "help-reports"
+oacensus help -on reports
+
+### "help-list"
+oacensus help -on list
+
+### "list-scrapers"
+oacensus list --scrapers
+
+### "list-reports"
+oacensus list --reports
--- examples/example1.sh
+++ examples/example1.sh
@@ -1,5 +1,11 @@
-### "scraper-docs"
-oacensus scrapers -alias pubmed
+### "pubmed-docs"
+oacensus list -alias pubmed
+
+### "doaj-docs"
+oacensus list -alias doaj
+
+### "oag-docs"
+oacensus list -alias oag
 
 ### "run-example"
-oacensus run --config example1.yaml
+oacensus run --config example1.yaml --reports excel
