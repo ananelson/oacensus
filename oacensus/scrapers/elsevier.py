e419189283a57a7f25242c476c13f4b9d9b757be
Ana Nelson <ana@ananelson.com>
2013-10-25 17:29:42 -0700

    Update Elsevier parser.

--- oacensus/scrapers/elsevier.py
+++ oacensus/scrapers/elsevier.py
@@ -1,12 +1,10 @@
 from bs4 import BeautifulSoup
 from oacensus.scraper import Scraper
-import glob
-import hashlib
-import json
+import cPickle as pickle
 import os
 import re
 import string
-import urllib
+import requests
 
 class ElsevierJournals(Scraper):
     """
@@ -16,133 +14,107 @@ class ElsevierJournals(Scraper):
 
     _settings = {
             "base-url" : ("Base url to scrape.", "http://www.elsevier.com/journals/title/"),
+            "data-file" : ("file to save data under", "elsevier-journal-list.html"),
             "non-alpha-pages" : (
-                "List of all pages named something other than a letter of the alphabet.",
+                "List of all pages to scrape which are named something other than a letter of the alphabet.",
                 ['other']
                 ),
-            "data-file" : ("file to save data under", "elsevier-journal-list.html")
             }
 
     def scrape(self):
+        journals = []
         pages = [l for l in string.ascii_lowercase] + self.setting('non-alpha-pages')
+
         for page in pages:
-            print "  fetching page", page
+            self.print_progress("  fetching page %s" % page)
             url = "%s%s" % (self.setting('base-url'), page)
-            filepath = os.path.join(self.work_dir(), "data-%s.html" % page)
-            urllib.urlretrieve(url, filepath)
 
-            skip = []
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text)
 
-            with open(filepath, 'r') as f:
-                soup = BeautifulSoup(f)
-                for anchor in self.journal_list_iter(soup):
-                    journal_url = anchor.get('href')
+            journal_list = soup.find('ul', class_="listing")
+            for entry in journal_list.findAll('li'):
+                anchor = entry.find('a')
+                journal_info = {}
+                issn_ok = False
 
-                    if "combined-subscription" in journal_url:
-                        continue
-
-                    if journal_url.startswith('/'):
-                        journal_url = "https://www.elsevier.com" + journal_url
-
-                    issn_found = False
-
-                    m = re.search("/([0-9a-z]{4}-[0-9a-z]{4})$", journal_url)
-                    if m:
-                        issn = m.groups()[0]
-                        issn_found = True
-                        print "  found issn", issn, "in url", journal_url
-                    else:
-                        journal_filename = hashlib.md5(journal_url).hexdigest()
-                        journal_filepath = os.path.join(self.work_dir(), journal_filename)
-
-                        n = 5
-
-                        for i in range(1, n):
-                            print "  fetching", journal_url, "attempt", i
-
-                            try:
-                                urllib.urlretrieve(journal_url, journal_filepath)
-                            except IOError:
-                                continue
-
-                            with open(journal_filepath, 'rb') as f:
-                                journal_soup = BeautifulSoup(f)
-                                if_table = journal_soup.select(".ifContainer .ifTD")
-                                if if_table:
-                                    issn_div = if_table[-1]
-                                    if "ISSN" in issn_div.text:
-                                        issn_found = True
-                                        break
-                                else:
-                                    if journal_soup.find('title').text == "Subjects | Elsevier":
-                                        skip.append(journal_url)
-                                        issn_found = True
-                                        break
-
-                    if not issn_found:
-                        raise Exception("Issn not found in %s after %s tries." % (journal_url, n))
-
-            skip_filepath = os.path.join(self.work_dir(), "skip.json")
-            with open(skip_filepath, 'w') as f:
-                json.dump(skip, f)
-
-
-    def journal_list_iter(self, soup):
-        journal_list = soup.find('ul', class_="listing")
-        for entry in journal_list.findAll('li'):
-            anchor = entry.find('a')
-            yield anchor
-
-    def process(self):
-        from oacensus.models import JournalList
-        from oacensus.models import Publisher
-        elsevier_list = JournalList.create(name="Elsevier Journals")
-        publisher = Publisher.create(name="Elsevier")
-
-        skip_filepath = os.path.join(self.cache_dir(), "skip.json")
-        with open(skip_filepath, 'r') as f:
-            skip_journals = json.load(f)
-
-        print "journals to skip are", skip_journals
-
-        for filepath in glob.iglob(os.path.join(self.cache_dir(), "data*.html")):
-            print "filename", filepath
-
-            with open(filepath, 'rb') as f:
-                soup = BeautifulSoup(f)
-
-            for anchor in self.journal_list_iter(soup):
                 journal_url = anchor.get('href')
 
                 if journal_url.startswith('/'):
                     journal_url = "https://www.elsevier.com" + journal_url
 
-                if journal_url in skip_journals:
-                    print "skipping", journal_url
+                journal_info['url'] = journal_url
+                journal_info['title'] = anchor.text.strip()
+
+                if "combined-subscription" in journal_url:
                     continue
 
-                m = re.search("/([0-9a-z]{4}-[0-9a-z]{4})$", journal_url)
-                if m:
-                    issn = m.groups()[0]
+                issn_match = re.search("/([0-9a-z]{4}-[0-9a-z]{4})$", journal_url)
+                if issn_match:
+                    self.print_progress("  don't need to fetch %s" % journal_url)
+                    issn_ok = True
+                    issn = issn_match.groups()[0]
+                    journal_info['issn'] = issn
+                    journals.append(journal_info)
 
                 else:
-                    journal_filename = hashlib.md5(journal_url).hexdigest()
-                    journal_filepath = os.path.join(self.cache_dir(), journal_filename)
+                    # ISSN is not in url, need to parse journal page.
+                    #
+                    # Pages don't always load correctly, so use multiple
+                    # attempts so one load failure doesn't mean we have to
+                    # start all over.
+                    attempts = 5
+                    for i in range(1, attempts):
+                        self.print_progress("  fetching %s attempt %s" % (journal_url, i))
 
-                    with open(journal_filepath, 'rb') as f:
-                        journal_soup = BeautifulSoup(f)
+                        try:
+                            response = requests.get(journal_url)
+                        except Exception as e:
+                            print e
+                            continue
+
+                        self.print_progress("  fetched. parsing...")
+                        journal_soup = BeautifulSoup(response.text)
                         if_table = journal_soup.select(".ifContainer .ifTD")
-                        issn_div = if_table[-1]
-                        if not "ISSN" in issn_div.text:
-                            raise Exception("no ISSN found in %s" % issn_div.text)
-                        issn = issn_div.text.replace("ISSN:", "").strip()
+                        if if_table:
+                            issn_div = if_table[-1]
+                            if "ISSN" in issn_div.text:
+                                issn_ok = True
+                                issn = issn_div.text.replace("ISSN:", "").strip()
+                                journal_info['issn'] = issn
+                                journals.append(journal_info)
+                                break
 
-                from oacensus.models import Journal
+                        else:
+                            if journal_soup.find('title').text == "Subjects | Elsevier":
+                                issn_ok = True
+                                break
+
+                    if not issn_ok:
+                        raise Exception("Issn not found in %s after %s tries." % (journal_url, attempts))
+
+        filepath = os.path.join(self.work_dir(), self.setting('data-file'))
+        with open(filepath, 'wb') as f:
+            pickle.dump(journals, f)
+
+    def process(self):
+        from oacensus.models import JournalList
+        from oacensus.models import Publisher
+        from oacensus.models import Journal
+
+        elsevier_list = JournalList.create(name="Elsevier Journals")
+        publisher = Publisher.create(name="Elsevier")
+
+        filepath = os.path.join(self.cache_dir(), self.setting('data-file'))
+        with open(filepath, 'rb') as f:
+            journals = pickle.load(f)
+
+        for journal_info in journals:
                 journal = Journal.create(
                         source=self.alias,
-                        title = anchor.text.strip(),
-                        issn=issn,
-                        url = journal_url,
+                        title = journal_info['title'],
+                        issn = journal_info['issn'],
+                        url = journal_info['url'],
                         publisher = publisher )
 
                 elsevier_list.add_journal(journal)
