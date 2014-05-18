5b1f8bd331b02658d7b4620bb17763fff13290a0
Ana Nelson <ana@ananelson.com>
2014-05-17 17:53:52 -0700

    Remove cache image.

--- docker/run-docker.sh
+++ docker/run-docker.sh
@@ -1,14 +1,8 @@
 ### "oacensus-dir"
 OACENSUS_DIR=~/dev/oacensus
 
-### "make-cache"
-docker build -t oacensus/cache - < Dockerfile-cache
-docker ps -a | grep OACENSUSCACHE || \
-    docker run --name=OACENSUSCACHE oacensus/cache
-
 ### "make-container"
 docker build -t oacensus/development . && \
     docker run -t -i \
-    --volumes-from=OACENSUSCACHE \
     -v $OACENSUS_DIR:/home/oacensus/oacensus \
     oacensus/development /bin/bash
