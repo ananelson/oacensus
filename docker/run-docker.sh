2c9d91a20ba9813a0ff66a0d54b1cd15ac0707d6
Ana Nelson <ana@ananelson.com>
2014-06-12 11:27:32 -0700

    Don't hard-code dir in docker script.

--- docker/run-docker.sh
+++ docker/run-docker.sh
@@ -1,8 +1,5 @@
-### "oacensus-dir"
-OACENSUS_DIR=~/dev/oacensus
-
 ### "make-container"
 docker build -t oacensus/development . && \
     docker run -t -i \
-    -v $OACENSUS_DIR:/home/oacensus/oacensus \
+    -v `pwd`/..:/home/oacensus/oacensus \
     oacensus/development /bin/bash
